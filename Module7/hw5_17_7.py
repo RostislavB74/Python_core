c701201e56934fcc626c9e04635f6c0039358ff2
Rostyslav Bysko <132708200+RostislavB74@users.noreply.github.com>
2023-06-12 19:39:44 +0300

    ok

---
+++ Module7/hw5_17_7.py
@@ -0,0 +1,46 @@
+def capital_text(s):
+    r = ''
+    symbol_list = ['.', '!', '?']
+    if len(s) == 0:
+        return False
+    if len(s) == 1:
+        new_str = s.capitalize()
+        return new_str
+    if len(s) >= 1:
+
+        s = s.capitalize()
+        i = 0
+        while i < len(s):
+            if s[i] in symbol_list:
+                r = r + s[i]+s[i+1]+s[i+2].title()
+                i += 3
+                continue
+            r += s[i]
+            i += 1
+        print(r)
+
+    return r
+
+    # if d[0] in symbol_list:
+    #     d = d.removeprefix(d[0])
+    #         #str_new = ''.join(new_d)
+    #         #try:
+    # if d.isdigit():
+    #     return True
+    # else:
+    #     return False
+
+    # print(d)
+
+    # dig = re.search('+', s)
+    # print(dig)
+
+    # if len(s) == 0:
+    #    return False
+    # if dig:
+    #    return True
+
+
+s = 'alert. boss! oh'  # 'Alert. Boss! Oh'
+
+print(capital_text(s))
