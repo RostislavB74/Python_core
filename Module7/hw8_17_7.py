186a0fc4420c61ae9110d9d3f01a146d98ce0e8a
Rostyslav Bysko <brn.h74@gmail.com>
2023-06-13 12:43:56 +0300

    edit

--- Module7/hw8_17_7.py
+++ Module7/hw8_17_7.py
@@ -1,37 +1,26 @@
 import re
 def token_parser(s):
-    #regex = r"\d+[\*,\/,\-,\+,\(,\)]+\d+[\*,\/,\-,\+,\(,\)]"
-    list_param_str =[]
-    lexema=['*', '/', '-', '+', '(', ')']
-    clean_s= s.replace(' ','')
-    #print(clean_s)
-    list_param_str1=re.findall('\d+',clean_s)
-    list_param_str=s.split(list_param_str1)
-
-    #match = clean_s.split(regex)
-    #print(match)
-    # for items in clean_s:
-
-    #     if items in lexema:
-    #         list_param_str= re.findall(items,clean_s)
-
-    #     else:
-    #         list_param_str1=re.findall('\d+',clean_s)
-    #print(list_param_str)
-    #print(list_param_str1)
-    #for items in clean_s:
+    if s:
+        list_param_str =[]
+        lexema=['*', '/', '-', '+', '(', ')']
+        clean_s= s.replace(' ','')
+        print(clean_s)
+        for i in clean_s:
+            if i in lexema:
+                print(i)
+            list_param_str.append(i)
+        for j in range(len(list_param_str)):
+            print(j)
+            
+        #list_param_str1=re.findall('\d+',clean_s)
+    
         
-    #    print ()
-
-        #list_param_str=clean_s.split(items)
-    print(list_param_str)
     
-
-
-    return list_param_str1
-    
+        return list_param_str
+    else:
+        return False
                 
             
-param_str= "2+ (34-5) * 3" # ['2', '+', '34', '-', '5', '*', '3']
+param_str= " 2+ (34-5) * 3 " # ['2', '+', '34', '-', '5', '*', '3']
 print (token_parser(param_str))
     
