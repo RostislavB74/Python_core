bae76981ce0a5f672ad86479bbb63a07e684aa28
Rostyslav Bysko <brn.h74@gmail.com>
2023-06-13 19:02:20 +0300

    ok

--- Module7/hw8_17_7.py
+++ Module7/hw8_17_7.py
@@ -2,21 +2,46 @@ import re
 def token_parser(s):
     if s:
         list_param_str =[]
+        new_list=[]
+        word=''
         lexema=['*', '/', '-', '+', '(', ')']
         clean_s= s.replace(' ','')
-        print(clean_s)
-        for i in clean_s:
-            if i in lexema:
-                print(i)
-            list_param_str.append(i)
-        for j in range(len(list_param_str)):
-            print(j)
+        for j in range(len(clean_s)):
+            
+            if clean_s[j] not in lexema:
+                word += clean_s[j]
+                #count = word
+                continue
+            else:
+                if word!='':
+                    new_list.append(word)
+                    new_list.append(clean_s[j])
+                    word=''
+                else:
+                    new_list.append(clean_s[j])
+                    word=''
+        #     for i in clean_s:
+            
+        #     if i not in lexema:
+        #         word +=i
+        #         continue
+        #     new_list.append(i)
+
+        # for j in range(len(list_param_str)):
+        #     if list_param_str[j] not in lexema:
+        #         word+=list_param_str[j]
+        #         new_list.append(word)
+        #     else:    
+        #         new_list.append(list_param_str[j])
+            
+        #print(new_list)
+
             
         #list_param_str1=re.findall('\d+',clean_s)
     
         
     
-        return list_param_str
+        return new_list
     else:
         return False
                 
