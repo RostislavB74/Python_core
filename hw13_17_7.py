e3acf2e6e8435654c0f1cf01c7a7b9314c771e44
Rostyslav Bysko <132708200+RostislavB74@users.noreply.github.com>
2023-06-14 21:57:28 +0300

    ok

--- hw13_17_7.py
+++ hw13_17_7.py
@@ -1,9 +1,21 @@
 def get_employees_by_profession(path, profession):
     with open(path, 'r') as fh:
+        new_list = []
         lines = fh.readlines()
-    print(lines)
+        lines = [line.rstrip() for line in lines]
+        for line in lines:
+            if line:
+                if line.find(profession) > 0:
+                    repl_str = line.replace(profession, '')
+                    # print(repl_str)
+                    new_str = repl_str.strip()
+                    new_list.append(new_str)
+                    stroka = " ".join(new_list)
+            else:
+                return
+        return stroka
 
 
 path = 'text_13_7.txt'
-profession = 'cook'
-get_employees_by_profession(path, profession)
+profession = 'courier'
+print(get_employees_by_profession(path, profession))
