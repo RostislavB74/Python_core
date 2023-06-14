db0cccf371de09b8809a66cea5a69931901fa4fc
Rostyslav Bysko <132708200+RostislavB74@users.noreply.github.com>
2023-06-14 19:43:18 +0300

    ok

--- Module7/hw11_17_7.py
+++ Module7/hw11_17_7.py
@@ -1,40 +1,41 @@
-phone={
-1:	['.', ',', '?', '!', ':',],
-2:	['A', 'B','C'],
-3:	['D', 'E', 'F'],
-4:	['G', 'H', 'I'],
-5:	['J', 'K', 'L'],
-6:	['M', 'N', 'O'],
-7:	['P', 'Q', 'R', 'S'],
-8:	['T', 'U', 'V'],
-9:	['W', 'X', 'Y', 'Z'],
-0:	[' ']
+phone = {
+    1:	['.', ',', '?', '!', ':',],
+    2:	['A', 'B', 'C'],
+    3:	['D', 'E', 'F'],
+    4:	['G', 'H', 'I'],
+    5:	['J', 'K', 'L'],
+    6:	['M', 'N', 'O'],
+    7:	['P', 'Q', 'R', 'S'],
+    8:	['T', 'U', 'V'],
+    9:	['W', 'X', 'Y', 'Z'],
+    0:	[' ']
 }
+
+
 def get_keys_num(bukva):
-        bukva=bukva.upper()
-        for key, values in phone.items():
-            if bukva in values:
-                return key, values
-        return key
+    bukva = bukva.upper()
+    num = ''
+    for key, values in phone.items():
+        if bukva in values:
+            for j in range(len(values)):
+                if bukva == values[j]:
+                    num = str(key)*(j+1)
+                    # print(num)
+            return num
+
+
 def sequence_buttons(string):
-    list_string=[]
+    list_string = []
+    trans = ''
     for i in string:
         list_string.append(i)
-    print (list_string)
-    for j in list_string: 
-        print(j)
-        print(get_keys_num(j))
+    # print(list_string)
+    for j in list_string:
+        # print(j)
+        trans += get_keys_num(j)
 
-        
-        
-        
+    return trans
 
 
-
-
-
-    return list_string
 string = "Hello, World!"
 print(sequence_buttons(string))
-        
-        
