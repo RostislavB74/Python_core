c55f410aae101dc5c2498d5b325cbbe95f50f535
Rostyslav Bysko <132708200+RostislavB74@users.noreply.github.com>
2023-06-14 20:10:11 +0300

    edit

---
+++ hw13_17_7.py
@@ -0,0 +1,11 @@
+def get_employees_by_profession(path, profession):
+     with open(path, 'r') as fh:
+        lines= fh.readlines()
+        
+
+
+
+
+    return path
+path = 'text_13_7.txt'
+get_employees_by_profession(path, profession)
