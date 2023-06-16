93c17b34f9d00feb729696daf155145530813a22
Rostyslav Bysko <brn.h74@gmail.com>
2023-06-16 07:55:06 +0300

    edit

--- Module7/hw15_17_7.py
+++ Module7/hw15_17_7.py
@@ -1,16 +1,17 @@
 data=[1, 2, [3, 4, [5, 6]], 7]
 new_list=[]
 def flatten(data):
-    if data:
-        for elem in data:
-            print (elem)
-            if elem is not list():
-                new_list.append(elem)
-                return new_list
-            else:
-                flatten(elem)
-    else:
-        return data
+    for elem in data:
+        #print (elem)
+        if elem is list():
+            for item in elem:
+                new_list.append(item)
+            return new_list   
+        else:
+            new_list.append(elem)
+            return new_list
+        
+                
    
     #return new_list
 
