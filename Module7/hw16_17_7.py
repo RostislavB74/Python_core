44a34224da7182fedeab1600acbb2e516fc303d2
Rostyslav Bysko <132708200+RostislavB74@users.noreply.github.com>
2023-06-18 09:38:42 +0300

    edit

--- Module7/hw16_17_7.py
+++ Module7/hw16_17_7.py
@@ -1,21 +1,24 @@
 list_data = ["X", "X", "X", "Z", "Z", "X", "X", "Y", "Y", "Y", "Z", "Z"]
 str_data = "XXXZZXXYYYZZ"
 
+new_list = []
+
 
 def decode(data):
-    counter = []
-    doubles = []
-    for elem in range(len(data)):
-        counter.append(elem[0])
-
-        counter[elem] = counter.get(elem, 0) + 1
-        doubles = {elem: count for element,
-                   count in counter.items() if count > 1}
-
-    return doubles
+    if data == []:
+        return data
+    new_list.append(data[0])
+    count = 1
+    for i in range(1, len(data)):
+        if data[i] == data[i-1]:
+            count += 1
+        else:
+            new_list.append(count)
+            new_list.append(data[i])
+            count = 1
+    new_list.append(count)
+    return new_list
 
 
 data = list_data
-print(decode(data))
-data = str_data
 print(decode(data))
