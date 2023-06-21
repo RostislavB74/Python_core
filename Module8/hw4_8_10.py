41cc46d72b8c3d69e88bd8360a793a391065a542
Rostyslav Bysko <brn.h74@gmail.com>
2023-06-21 15:47:40 +0300

    edit

---
+++ Module8/hw4_8_10.py
@@ -0,0 +1,10 @@
+from random import randrange
+min = 1 # 1
+max = 36 # 5x36 # 6x49
+quantity = 36
+
+def get_numbers_ticket(min, max, quantity):
+    
+    randrange(min, max, quantity)
+    
+get_numbers_ticket(min, max, quantity)
