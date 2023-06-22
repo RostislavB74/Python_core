027a8ed7f166062d58a4bc2d6484cb9355bfaed7
Rostyslav Bysko <brn.h74@gmail.com>
2023-06-22 08:21:23 +0300

    ok

--- Module8/hw4_8_10.py
+++ Module8/hw4_8_10.py
@@ -1,39 +1,20 @@
 import random
 from random import randrange
-<<<<<<< HEAD
 min = 1 # 1
-max = 36 # 5x36 # 6x49
-quantity = 36
+max = 49 # 5x36 # 6x49
+quantity = 6
 
 def get_numbers_ticket(min, max, quantity):
-    set_dig=[]
-    set_num = {randrange(min, max) for i in range(quantity) }
-    print(set_num)
-    set_dig=random.sample(list(set_num), k=5)
-    
-    return set_dig
-    
-print(get_numbers_ticket(min, max, quantity))
-=======
-min = 1  # 1
-max = 100  # 5x36 # 6x49
-quantity = 49
-
-
-def get_numbers_ticket(min, max, quantity):
-
-    set_dig = []
-    set_list = []
-    if (min >= 1) and (max < 1000):
-        if (quantity >= min) and (quantity < max):
-            set_num = {randrange(min, max) for i in range(max)}
-            # print(set_num)
-            set_list = list(set_num)
-            set_dig = random.sample(set_list, k=quantity)
-            # print(sorted(set_list))
+    if min>=1 and max<1000:
+        set_dig=[]
+        if quantity>=min and quantity<max: 
+            set_dig=[]
+            #set_list=[]
+            num_list = [randrange(min, max) for i in range(max) ]
+            #print(num_list)
+            set_dig=random.sample(num_list, k=quantity)
+            print(set_dig)
             return sorted(set_dig)
-    return
-
-
+    return []
+    
 print(get_numbers_ticket(min, max, quantity))
->>>>>>> 39e7e6eb4731e30ba051a1b6dd28861eb357268c
