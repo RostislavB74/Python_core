59daff383cae31c1e11d6bf2afb8ad0f88479111
Rostyslav Bysko <brn.h74@gmail.com>
2023-06-22 08:17:58 +0300

    ok

--- Module8/hw4_8_10.py
+++ Module8/hw4_8_10.py
@@ -1,10 +1,15 @@
+import random
 from random import randrange
 min = 1 # 1
 max = 36 # 5x36 # 6x49
 quantity = 36
 
 def get_numbers_ticket(min, max, quantity):
+    set_dig=[]
+    set_num = {randrange(min, max) for i in range(quantity) }
+    print(set_num)
+    set_dig=random.sample(list(set_num), k=5)
     
-    randrange(min, max, quantity)
+    return set_dig
     
-get_numbers_ticket(min, max, quantity)
+print(get_numbers_ticket(min, max, quantity))
