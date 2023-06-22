8b93c8f31ed69cf5d35cc4290ad12e0c416285ff
Rostyslav Bysko <brn.h74@gmail.com>
2023-06-22 16:24:30 +0300

    edit

---
+++ Module8/hw7_8_10.py
@@ -0,0 +1,30 @@
+import collections
+
+
+Cat = collections.namedtuple("Cat", ["nickname", "age", "owner"])
+#cats_dict=collections.namedtuple("nickname":'nickname', "age":'age', "owner":'owner')
+cats=[Cat("Mick", 5, "Sara"), Cat("Barsik", 7, "Olga"), Cat("Simon", 3, "Yura")]
+
+def convert_list(cats):
+    if isinstance(cats, list):
+        cats_list=[]
+        for i in cats:
+            cats_dict={}
+            cats_dict["nickname"]=(i.nickname)
+            cats_dict["age"]=(i.age)
+            cats_dict["owner"]=(i.owner)
+            cats_list.append(cats_dict)
+        return(cats_list)
+    # if isinstance(cats, dict):
+    #        for i in cats:
+    #         cats_dict=[]
+    #         cats_dict["nickname"]=(i.nickname)
+    #         cats_dict["age"]=(i.age)
+    #         cats_dict["owner"]=(i.owner)
+    #         cats_list.append(cats_dict)
+    #     return(cats_list)
+            
+convert_list(cats)
+            
+        
+    
