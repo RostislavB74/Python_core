f636795085005ea06fc9da61d2e2caadd8d7254a
Rostyslav Bysko <brn.h74@gmail.com>
2023-06-22 08:21:31 +0300

    ok

--- Module8/hw3_8_10.py
+++ Module8/hw3_8_10.py
@@ -1,16 +1,20 @@
 from datetime import datetime
-date = '2021-05-27 17:08:34.149Z'
+date='2021-05-27 17:08:34.149Z'
 
 
 def get_str_date(date):
 
-    data = date.split(' ')
-
+    data= date.split(' ')
+    
     date_1 = datetime.strptime(data[0], '%Y-%m-%d').date()
+    
+    #print(date_1.strftime('%A %d %B %Y')) # Tuesday 07 January 2020    
 
-    print(date_1.strftime('%A %d %B %Y'))  # Tuesday 07 January 2020
 
     return date_1.strftime('%A %d %B %Y')
 
 
 get_str_date(date)
+    
+    
+    
