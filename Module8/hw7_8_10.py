89d6cef954042221d48bcbc0aae141a9235bf0bd
Rostyslav Bysko <brn.h74@gmail.com>
2023-06-23 14:32:39 +0300

    ok

--- Module8/hw7_8_10.py
+++ Module8/hw7_8_10.py
@@ -1,28 +1,32 @@
 import collections
 
-
+#[Cat(nickname='Mick', age=5, owner='Sara'), Cat(nickname='Barsik', age=7, owner='Olga'), Cat(nickname='Simon', age=3, owner='Yura')]
 Cat = collections.namedtuple("Cat", ["nickname", "age", "owner"])
+Cat1 = collections.namedtuple("Cat", ["nickname", "age", "owner"])
 #cats_dict=collections.namedtuple("nickname":'nickname', "age":'age', "owner":'owner')
-cats=[Cat("Mick", 5, "Sara"), Cat("Barsik", 7, "Olga"), Cat("Simon", 3, "Yura")]
-
+#cats=[Cat("Mick", 5, "Sara"), Cat("Barsik", 7, "Olga"), Cat("Simon", 3, "Yura")]
+cats=[{'nickname': 'Mick', 'age': 5, 'owner': 'Sara'}, {'nickname': 'Barsik', 'age': 7, 'owner': 'Olga'}, {'nickname': 'Simon', 'age': 3, 'owner': 'Yura'}]
 def convert_list(cats):
-    if isinstance(cats, list):
-        cats_list=[]
-        for i in cats:
+   
+    cats_list=[]
+    for i in cats:
+        if isinstance(i, tuple):
             cats_dict={}
             cats_dict["nickname"]=(i.nickname)
             cats_dict["age"]=(i.age)
             cats_dict["owner"]=(i.owner)
             cats_list.append(cats_dict)
-        print(cats_list)
-            # print(i.owner)
-        
-
+        if isinstance(i, dict):
+            cats2=[]
+            cats2=Cat(**i)
             
-        #print(cats.Cat)
+            cats_list.append(cats2)
+           
         
+    return cats_list
+           
             
-convert_list(cats)
+print(convert_list(cats))
             
         
     
