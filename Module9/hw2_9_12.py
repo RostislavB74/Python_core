95a77d79af4aa4655c56c6f77a35150e52d30932
Rostyslav Bysko <132708200+RostislavB74@users.noreply.github.com>
2023-06-28 22:03:41 +0300

    edit

--- Module9/hw2_9_12.py
+++ Module9/hw2_9_12.py
@@ -1,20 +1,26 @@
 DEFAULT_DISCOUNT = 0.05
-customer = [{'name': "Dima"}, {'name': 'Boris', 'discount': 0.15}]
+customer_list = [{'name': "Dima"}, {'name': 'Boris', 'discount': 0.15}]
 price = 100
 
 
 def get_discount_price_customer(price, customer):
-    for elem in customer:
-        for keys in elem:
+    for customer in customer_list:
+        for keys in customer:
+            name = keys[0]
             if keys == 'discount':
-                if elem[keys]:
-                    price = price * (1 - elem['discount'])
-                    return price
-            price = price * (1 - DEFAULT_DISCOUNT)
+                if customer_list[keys]:
+                    price = price * (1 - customer_list['discount'])
+                    print(f'{name}: {price}')
+            else:
+
+                price = price * (1 - DEFAULT_DISCOUNT)
+                print(f'{name}: {price}')
+            continue
             # if key and values:
 
             #     price = price * (1 - )
-            return price
+    return price
 
 
+customer = 'Dima'
 print(get_discount_price_customer(price, customer))
