24510b44eb563dc8fd2443558157c6f00347ecbb
Rostyslav Bysko <132708200+RostislavB74@users.noreply.github.com>
2023-07-01 20:55:35 +0300

    edit

---
+++ Module9/hw4_9_12.py
@@ -0,0 +1,23 @@
+price = 100
+
+
+def discount_price(discount):
+    result = price * (1 - discount)
+    print(result)
+    return result
+
+
+def main():
+    calc = discount_price()
+    print(cost_15(price))
+    print(cost_10(price))
+    print(cost_05(price))
+
+
+cost_15 = discount_price(0.15)
+cost_10 = discount_price(0.10)
+cost_05 = discount_price(0.05)
+
+
+if __name__ == '__main__':
+    main()
