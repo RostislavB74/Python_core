a5c383709332d8a5b3e310a6e9ca48c9e0d6cc8d
Rostyslav Bysko <132708200+RostislavB74@users.noreply.github.com>
2023-07-01 22:51:12 +0300

    edit

--- Module9/hw4-9-12-1.py
+++ Module9/hw4-9-12-1.py
@@ -2,29 +2,21 @@ price = 100
 
 
 def discount_price(discount):
-    result = price * (1 - discount)
+    result = price * (1 - discount*0.01)
     print(result)
     return result
 
-
-DISCOUNT = {
-    'cost_15': 0.15,
-    'cost_10': 0.1,
-    'cost_05': 0.05
-}
-
-
-def main():
-    calc = discount_price()
-    print(calc(0.15))
-    print(calc(0.10))
-    print(calc(0.05))
+# def main():
+    # calc = discount_price()
+    # print(cost_15(15))
+    # print(cost_10(10))
+    # print(cost_05(5))
 
 
-cost_15 = discount_price(0.15)
-cost_10 = discount_price(0.10)
-cost_05 = discount_price(0.05)
+cost_15 = discount_price(15)
+cost_10 = discount_price(10)
+cost_05 = discount_price(5)
 
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
