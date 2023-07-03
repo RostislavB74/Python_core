36f61d4aa735e988d846f009bceaa58e31154178
Rostyslav Bysko <132708200+RostislavB74@users.noreply.github.com>
2023-07-03 22:18:11 +0300

    edit

--- Module9/hw6_9_12.py
+++ Module9/hw6_9_12.py
@@ -3,20 +3,27 @@ import re
 
 def generator_numbers(string):
     str_list = string.split(' ')
-    while i <= len(str_list):
-        numbers.append(re.findall('\d+', elem[i]))
-        i += 1
-
-
-def sum_profit(string):
+    # for i in len(str_list)
+    numbers = []
     result = 0
-    for elem in numbers:
-        result += elem
+
+    for elem in str_list:
+        # print(elem)
+        if re.findall('\d+', elem):
+            res_1 = re.findall('\d+', elem)
+            result += int(sum_profit(res_1))
     return result
 
 
-string = "The resulting profit was: from the southern possessions $ 100, from the northern colonies $500, and the king gave $1000."
+def sum_profit(res_1):
+    res = int(res_1[0])
 
+    # print(res)
+    return res
+
+
+# string = "The resulting profit was: from the southern pos200sessions 100$ 100, from the northern colonies $500, and the king gave $1000."
+string = "The resulting profit was: from the southern possessions $ 10, from the northern colonies $50, and the king gave $100."
 
 result = generator_numbers(string)
 print(result)
