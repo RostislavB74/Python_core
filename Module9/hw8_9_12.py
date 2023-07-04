fb305d3edcd2e4383463357c2c06962e754d28e4
Rostyslav Bysko <132708200+RostislavB74@users.noreply.github.com>
2023-07-04 19:29:42 +0300

    edit

---
+++ Module9/hw8_9_12.py
@@ -0,0 +1,21 @@
+user = {
+    "name": "Allen Raymond",
+    "email": "nulla.ante@vestibul.co.uk",
+    "phone": "(992) 914-3792",
+    "favorite": False,
+}
+
+
+new_list = []
+
+
+def get_emails(list_contacts):
+    # for i in map(lambda elem: new_list.append(elem['email']), user):
+    for i in map(lambda elem: print(elem), list_contacts.items()):
+        print()
+        # print(new_list)
+
+        # return (new_list)
+
+
+print(get_emails(user))
