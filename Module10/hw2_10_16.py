1d6062cd9ed5e2cb1a953b4b917de3d61e3ec5af
Rostyslav Bysko <brn.h74@gmail.com>
2023-07-05 08:00:12 +0300

    ok

---
+++ Module10/hw2_10_16.py
@@ -0,0 +1,13 @@
+class Animal:
+    def __init__(self, nickname, weight):
+        self.nickname = nickname
+        self.weight = weight
+    def say(self) :
+        pass
+        
+     
+         
+animal = Animal ("bobik", 1)
+
+    
+        
