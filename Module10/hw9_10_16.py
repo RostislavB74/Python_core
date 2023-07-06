f2dc8a4431f1c7743048ea6bbd6d04c2d02f5b9e
Rostyslav Bysko <132708200+RostislavB74@users.noreply.github.com>
2023-07-06 22:08:38 +0300

    edit

--- Module10/hw9_10_16.py
+++ Module10/hw9_10_16.py
@@ -2,19 +2,18 @@ from collections import UserDict
 
 
 class UserDict(UserDict):
-    # def __init__(self, data):
-    data = dict()
+    def __init__(self, data):
+        self.dict = data
 
 
 class LookUpKeyDict(UserDict):
     def __init__(self, value):
-        super().__init__(dict)
+        super().__init__(data)
         self.value = value
-        self.data = dict
 
     def lookup_key(self, data, value):
         keys = []
-        for i in self.data:
+        for i in data:
             print(i)
             if i == value:
                 keys.append(i)
@@ -25,6 +24,6 @@ class LookUpKeyDict(UserDict):
 as_dict = UserDict()
 as_dict['a'] = 1
 as_dict['b'] = 2
-y = LookUpKeyDict()
+y = LookUpKeyDict('v')
 y.lookup_key = (as_dict, 'v')
 print(y.lookup_key())
