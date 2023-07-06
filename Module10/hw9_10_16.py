3e5f1f1fb0d9d35533715448f6359adf5a5304c1
Rostyslav Bysko <brn.h74@gmail.com>
2023-07-06 16:09:43 +0300

    edit

--- Module10/hw9_10_16.py
+++ Module10/hw9_10_16.py
@@ -12,26 +12,26 @@ class LookUpKeyDict(UserDict):
         self.data = data
         super().__init__(data)
         
-    def lookup_key(self, key, data):
+    def lookup_key(self, value, data):
         keys = []
         for key in data:
             if data[key] == value:
                 keys.append(key)
-        return keys
+        return print(keys)
 
 
 as_dict = UserDict()
-as_dict['a'] = 1
-as_dict['b'] = 2
-print(as_dict.data)
-key1=LookUpKeyDict()
+as_dict.data['a'] = 1
+as_dict.data['b'] = 2
+
+key1=LookUpKeyDict(self)
 key1.lookup_key=('v', as_dict)
-print(key1.lookup_key())
 
-#keys_list =LookUpKeyDict()
 
-#print(keys_list.lookup_key())
-#data1=LookUpKeyDict()
-#data1.lookup_key['b']=2
+keys_list =LookUpKeyDict()
+
+print(keys_list.lookup_key())
+data1=LookUpKeyDict()
+data1.lookup_key['b']=2
 
 
