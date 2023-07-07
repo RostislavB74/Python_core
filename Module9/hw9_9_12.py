6476898b635f79c1799597007613199e24a5209a
Rostyslav Bysko <132708200+RostislavB74@users.noreply.github.com>
2023-07-07 19:57:33 +0300

    ok

--- Module9/hw9_9_12.py
+++ Module9/hw9_9_12.py
@@ -1,11 +1,20 @@
 payment = [100, -3, 400, 23, 35, -100]
 
 
-def positive_values(list_payment):
-    list1 = []
-    for i in filter(lambda x: x > 0, payment):
-        list1.append(i)
-    return list1
+# def positive_values(list_payment):
+#     list1 = []
+#     for i in filter(lambda x: x > 0, payment):
+#         list1.append(i)
+#     return list1
+
+
+def is_positive(num):
+    return num >= 0
+
+
+def positive_values(payment):
+    list1 = filter(is_positive, payment)
+    return list(list1)
 
 
 print(positive_values(payment))
