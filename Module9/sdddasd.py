d6e7e2c7fc3f6a2b7ed8a018cf5b12a88db9ca88
Rostyslav Bysko <brn.h74@gmail.com>
2023-07-07 14:59:57 +0300

    ok

---
+++ Module9/sdddasd.py
@@ -0,0 +1,22 @@
+users=[{'name': 'Allen Raymond', 'email': 'nulla.ante@vestibul.co.uk', 'phone': '(992) 914-3792', 'favorite': False}, {'name': 'Chaim Lewis', 'email': 'dui.in@egetlacus.ca', 'phone': '(294) 840-6685', 'favorite': False}, {'name': 'Kennedy Lane', 'email': 'mattis.Cras@nonenimMauris.net', 'phone': '(542) 451-7038', 'favorite': False}, {'name': 'Wylie Pope', 'email': 'est@utquamvel.net', 'phone': '(692) 802-2949', 'favorite': False}, {'name': 'Cyrus Jackson', 'email': 'nibh@semsempererat.com', 'phone': '(501) 472-5218', 'favorite': False}] 
+
+def get_emails(list_contacts):
+    new_list=[]
+    for elem in list_contacts:
+        return elem['email']
+def add_email(users):
+    return list(map(get_emails,users))
+    #print(res)
+print(add_email(users))
+
+
+
+
+# def is_positive(num):
+#    return num >= 0
+
+# def sanitized_sqrt(numbers):
+#      cleaned_iter = map(math.sqrt, filter(is_positive, numbers))
+#      return list(cleaned_iter)
+
+# sanitized_sqrt([25, 9, 81, -16, 0])
---
+++ Module9/waaa111.py
@@ -0,0 +1,11 @@
+users=[{'name': 'Allen Raymond', 'email': 'nulla.ante@vestibul.co.uk', 'phone': '(992) 914-3792', 'favorite': False}, {'name': 'Chaim Lewis', 'email': 'dui.in@egetlacus.ca', 'phone': '(294) 840-6685', 'favorite': False}, {'name': 'Kennedy Lane', 'email': 'mattis.Cras@nonenimMauris.net', 'phone': '(542) 451-7038', 'favorite': False}, {'name': 'Wylie Pope', 'email': 'est@utquamvel.net', 'phone': '(692) 802-2949', 'favorite': False}, {'name': 'Cyrus Jackson', 'email': 'nibh@semsempererat.com', 'phone': '(501) 472-5218', 'favorite': False}] 
+
+def get_emails(list_contacts):
+    new_list=[]
+    for elem in list_contacts:
+        new_list.append(elem['email'])
+    return new_list
+def add_email(users):
+    res =get_emails(users)
+    return res 
+print(add_email(users))
