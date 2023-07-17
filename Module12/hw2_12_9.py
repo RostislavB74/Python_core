fce271dc95cd00160239bea69f9bfad0479fe166
Rostyslav Bysko <brn.h74@gmail.com>
2023-07-17 18:36:00 +0300

    edit

--- Module12/hw2_12_9.py
+++ Module12/hw2_12_9.py
@@ -1,4 +1,5 @@
 import json
+import sys
 contacts= {'name': 'Allen Raymond', 'email': 'nulla.ante@vestibul.co.uk', 'phone': '(992) 914-3792', 'favorite': False}, {'name': 'Chaim Lewis', 'email': 'dui.in@egetlacus.ca', 'phone': '(294) 840-6685', 'favorite': False}
 #contacts = {'key': 'value', 2: [1, 2, 3], 'tuple': (5, 6), 'a': {'key': 'value'}}
 filename = 'data.json'
