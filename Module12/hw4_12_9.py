75a107b0e277c385db506851b0e3c6298773d000
Rostyslav Bysko <132708200+RostislavB74@users.noreply.github.com>
2023-07-18 18:35:33 +0300

    edit

---
+++ Module12/hw4_12_9.py
@@ -0,0 +1,44 @@
+import pickle
+
+
+class Person:
+    def __init__(self, name: str, email: str, phone: str, favorite: bool):
+        ...
+
+
+contacts = [
+    Person(
+        "Allen Raymond",
+        "nulla.ante@vestibul.co.uk",
+        "(992) 914-3792",
+        False,
+    ),
+    Person(
+        "Chaim Lewis",
+        "dui.in@egetlacus.ca",
+        "(294) 840-6685",
+        False,
+    ),
+]
+
+
+class Contacts:
+    def __init__(self, filename: str, contacts: list[Person] = None):
+        if contacts is None:
+            contacts = []
+
+    def save_to_file(self):
+        with open(filename, "wb") as fh:
+            pickle.dump(contacts, fh)
+
+    def read_from_file(self):
+        with open(filename, "rb") as fh:
+            unpack_contacts = pickle.load(fh)
+
+
+filename = "user_class.dat"
+# bob = Human("Bob")
+# encoded_bob = pickle.dumps(bob)
+
+# decoded_bob = pickle.loads(encoded_bob)
+persons = Contacts("user_class.dat", contacts)
