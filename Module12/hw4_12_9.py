eb67c1b2f7f0013f4a8a5dcbca781d5d819b9e85
Rostyslav Bysko <brn.h74@gmail.com>
2023-07-20 08:10:47 +0300

    edit

--- Module12/hw4_12_9.py
+++ Module12/hw4_12_9.py
@@ -1,13 +1,13 @@
 import pickle
 
-
+filename = 'user_class.dat'
 class Person:
     def __init__(self, name: str, email: str, phone: str, favorite: bool):
         self.name = name   
         self.email= email
         self.phone = phone
         self.favorite = favorite
-        return self
+        
         
         
         
@@ -22,13 +22,15 @@ class Contacts:
         
 
     def save_to_file(self):
-        with open(self.filename, "wb") as fh:
+        with open(filename, "wb") as fh:
             pickle.dump(self.contacts, fh)
         
     def read_from_file(self):
-        with open(self.filename, "rb") as fh:
+        with open(filename, "rb") as fh:
             unpack_contacts = pickle.load(fh) 
-        return unpack_contacts        
+            return unpack_contacts
+            
+                
 
 contacts = [
     Person("Allen Raymond",
@@ -42,4 +44,15 @@ contacts = [
         False,
     ),
 ]    
-persons = Contacts("user_class.dat", contacts)
+persons = Contacts(filename, contacts)
+print(f"persons_do{persons}")
+
+#persons = Contacts("user_class.dat", contacts)
+persons.save_to_file()
+person_from_file = persons.read_from_file()
+print(f"persons_posle{person_from_file}")
+print(persons == person_from_file)  # False
+print(persons.contacts[0] == person_from_file.contacts[0])  # False
+#print(persons.contacts[0].name == person_from_file.contacts[0].name)  # True
+#print(persons.contacts[0].email == person_from_file.contacts[0].email)  # True
+#print(persons.contacts[0].phone == person_from_file.contacts[0].phone)  # True
