5550b109f543c4973aac33816643fec25cf2a14d
Rostyslav Bysko <brn.h74@gmail.com>
2023-07-31 07:59:12 +0300

    ok

---
+++ Module12/hw_9_12_9.py
@@ -0,0 +1,51 @@
+import copy
+import pickle
+
+class Person:
+    def __init__(self, name: str, email: str, phone: str, favorite: bool):
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.favorite = favorite
+
+    def __copy__(self):
+        return Person(self.name, self.email, self.phone, self.favorite)
+
+    def __deepcopy__(self, memo):
+        return Person(self.name, self.email, self.phone, self.favorite)
+
+
+class Contacts:
+    def __init__(self, filename: str, contacts: list):
+        # self.filename = filename
+        # self.contacts = contacts
+        self.filename = filename
+        self.contacts = contacts
+        self.is_unpacking = False
+        self.count_save = 0
+
+    def save_to_file(self):
+        with open(self.filename, "wb") as file:
+            pickle.dump(self, file)
+
+    def read_from_file(self):
+        with open(self.filename, "rb") as file:
+            content = pickle.load(file)
+        return content
+    def __getstate__(self):
+        attributes = self.__dict__.copy()
+        attributes["count_save"] = attributes["count_save"] + 1
+        return attributes
+
+    def __setstate__(self, value):
+        self.__dict__ = value
+        self.is_unpacking = True
+
+    
+    def __copy__(self):
+        return Contacts(self.filename, self.contacts[:])
+
+    def __deepcopy__(self, memo):
+        copied_contacts = Contacts(self.filename, copy.deepcopy(self.contacts, memo))
+        memo[id(self)] = copied_contacts
+        return copied_contacts
